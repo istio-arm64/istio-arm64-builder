6672560e75ddb087b9e48237dbe904aebb9c3e41
钱卫春 <qianwch@chinasofti.com>
2020-11-09 22:40:07 +0800

    gzip the envoy binary

--- proxy-builder/build.sh
+++ proxy-builder/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-case $(uname -m) in 
+case $(uname -m) in
 	x86_64) export TARGET_ARCH=amd64;;
 	aarch64) export TARGET_ARCH=arm64;;
 	*) echo "cpu NOT in support list"; exit 1 ;;
@@ -15,4 +15,5 @@ BIN_FULLNAME=envoy-${REV_SHA1}-${TARGET_ARCH}
 BAZEL_BUILD_ARGS="$BAZEL_BUILD_ARGS -c opt" make build_envoy
 
 cp /work/proxy/bazel-bin/src/envoy/envoy /build/envoy
-cp /build/envoy /build/$BIN_FULLNAME
+tar -C /build -czf /build/${BIN_FULLNAME}.tar.gz envoy
+
