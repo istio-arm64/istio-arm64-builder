95f0589e3a444b4475952f0656db8d586f7f99c0
钱卫春 <qianwch@chinasofti.com>
2020-11-30 14:08:57 +0800

    support 1.8.0 fix3

--- proxy-builder/build.sh
+++ proxy-builder/build.sh
@@ -17,7 +17,7 @@ REV=$(git rev-parse $ISTIO_VERSION)
 BAZEL_OUT="$(bazel info $BAZEL_BUILD_ARGS -c opt output_path)/k8-opt/bin"
 BAZEL_TARGET="${BAZEL_OUT}/src/envoy/envoy_tar.tar.gz"
 BIN_FULLNAME=istio-proxy-${ISTIO_VERSION}-${TARGET_ARCH}
-bazel build ${BAZEL_BUILD_ARGS} --config=release -c opt //src/envoy:envoy_tar
+bazel build ${BAZEL_BUILD_ARGS} --config=libc++ --config=release -c opt //src/envoy:envoy_tar
 cp -f "${BAZEL_TARGET}" /build/envoy-${REV}.tar.gz
 
 if [[ $ISTIO_VERSION =~ ^1\.8 ]]; then
@@ -25,6 +25,6 @@ if [[ $ISTIO_VERSION =~ ^1\.8 ]]; then
   BAZEL_OUT="$(bazel info $BAZEL_BUILD_ARGS -c opt --cxxopt -D_GLIBCXX_USE_CXX11_ABI=1 --cxxopt -DENVOY_IGNORE_GLIBCXX_USE_CXX11_ABI_ERROR=1 output_path)/k8-opt/bin"
 	BAZEL_TARGET="${BAZEL_OUT}/src/envoy/envoy_tar.tar.gz"
 	CENTOS_BIN_FULLNAME=istio-proxy-centos-${ISTIO_VERSION}-${TARGET_ARCH}
-	bazel build ${BAZEL_BUILD_ARGS} --config=release -c opt --cxxopt -D_GLIBCXX_USE_CXX11_ABI=1 --cxxopt -DENVOY_IGNORE_GLIBCXX_USE_CXX11_ABI_ERROR=1 //src/envoy:envoy_tar
+	bazel build ${BAZEL_BUILD_ARGS} --config=libc++ --config=release -c opt --cxxopt -D_GLIBCXX_USE_CXX11_ABI=1 --cxxopt -DENVOY_IGNORE_GLIBCXX_USE_CXX11_ABI_ERROR=1 //src/envoy:envoy_tar
 	cp -f "${BAZEL_TARGET}" /build/envoy-centos-${REV}.tar.gz
 fi
