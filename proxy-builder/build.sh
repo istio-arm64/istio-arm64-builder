8ad59cdd9e0f6e25514ef1b72bfc1b11afe7cb3c
钱卫春 <qianwch@chinasofti.com>
2020-11-30 12:10:51 +0800

    support 1.8.x

--- proxy-builder/build.sh
+++ proxy-builder/build.sh
@@ -10,10 +10,18 @@ esac
 
 git clone --depth=1 -b $ISTIO_VERSION https://github.com/istio/proxy.git
 cd proxy
+REV=$(git rev-parse $ISTIO_VERSION)
+
 BIN_FULLNAME=istio-proxy-${ISTIO_VERSION}-${TARGET_ARCH}
 BAZEL_BUILD_ARGS="$BAZEL_BUILD_ARGS -c opt" make build_envoy
-BAZEL_BUILD_ARGS="$BAZEL_BUILD_ARGS --cxxopt -D_GLIBCXX_USE_CXX11_ABI=1 --cxxopt -DENVOY_IGNORE_GLIBCXX_USE_CXX11_ABI_ERROR=1 -c opt" BUILD_ENVOY_BINARY_ONLY=1 BASE_BINARY_NAME=envoy-centos make build_envoy
-
 cp /work/proxy/bazel-bin/src/envoy/envoy /build/envoy
+cp /build/envoy /build/envoy-$REV
 tar -C /build -czf /build/${BIN_FULLNAME}.tar.gz envoy
 
+if [[ $ISTIO_VERSION =~ ^1\.8 ]]; then
+	CENTOS_BIN_FULLNAME=istio-proxy-centos-${ISTIO_VERSION}-${TARGET_ARCH}
+	BAZEL_BUILD_ARGS="$BAZEL_BUILD_ARGS -c opt --cxxopt -D_GLIBCXX_USE_CXX11_ABI=1 --cxxopt -DENVOY_IGNORE_GLIBCXX_USE_CXX11_ABI_ERROR=1" BUILD_ENVOY_BINARY_ONLY=1 BASE_BINARY_NAME=envoy-centos make build_envoy
+	cp /work/proxy/bazel-bin/src/envoy/envoy-centos /build/envoy-centos
+	cp /build/envoy-centos /build/envoy-centos-$REV
+	tar -C /build -czf /build/${CENTOS_BIN_FULLNAME}.tar.gz envoy-centos
+fi
